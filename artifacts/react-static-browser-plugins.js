3e584d448c65d059a529ed0ddbaa2f38815577b2
Mounir Benabderrahmane <bnmounir7@gmail.com>
2020-03-22 15:07:29 -0400

    artifacts

---
+++ artifacts/react-static-browser-plugins.js
@@ -0,0 +1,27 @@
+// Imports
+import plugin0 from '/Users/macbook/Desktop/portfolio/node_modules/react-static-plugin-reach-router/browser.api.js'
+
+// Plugins
+const plugins = [{
+        location: "/Users/macbook/Desktop/portfolio/node_modules/react-static-plugin-source-filesystem",
+        plugins: [],
+        hooks: {}
+      },
+{
+        location: "/Users/macbook/Desktop/portfolio/node_modules/react-static-plugin-reach-router",
+        plugins: [],
+        hooks: plugin0({})
+      },
+{
+        location: "/Users/macbook/Desktop/portfolio/node_modules/react-static-plugin-sitemap/dist",
+        plugins: [],
+        hooks: {}
+      },
+{
+        location: "/Users/macbook/Desktop/portfolio",
+        plugins: [],
+        hooks: {}
+      }]
+
+// Export em!
+export default plugins
---
+++ artifacts/react-static-templates.js
@@ -0,0 +1,13 @@
+
+
+  
+// Template Map
+export default {
+  '/Users/macbook/Desktop/portfolio/src/pages/404.js': require('/Users/macbook/Desktop/portfolio/src/pages/404.js').default,
+'/Users/macbook/Desktop/portfolio/src/pages/about.js': require('/Users/macbook/Desktop/portfolio/src/pages/about.js').default,
+'/Users/macbook/Desktop/portfolio/src/pages/index.js': require('/Users/macbook/Desktop/portfolio/src/pages/index.js').default,
+'/Users/macbook/Desktop/portfolio/src/pages/projects.js': require('/Users/macbook/Desktop/portfolio/src/pages/projects.js').default
+}
+
+export const notFoundTemplate = '/Users/macbook/Desktop/portfolio/src/pages/404.js'
+
